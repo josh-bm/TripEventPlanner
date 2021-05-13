ab4baf6b9eebef9b2983349fdd25fcf3a07f9dd3
Baulukanda <42438450+Baulukanda@users.noreply.github.com>
2021-05-13 15:07:49 +0200

    images is here

--- TripEventPlanner/Models/ViewModels/TripViewModel.cs
+++ TripEventPlanner/Models/ViewModels/TripViewModel.cs
@@ -7,5 +7,9 @@ namespace TripEventPlanner.Models.ViewModels
 {
     public class TripViewModel
     {
+        public string Name { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
