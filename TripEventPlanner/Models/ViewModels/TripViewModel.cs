54aca97eccebbf7998e5cb74db74151eed215583
joshbm-git <joshbm.dev@gmail.com>
2021-05-13 13:44:15 +0200

    foreach structure in trips page

---
+++ TripEventPlanner/Models/ViewModels/TripViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripEventPlanner.Models.ViewModels
+{
+    public class TripViewModel
+    {
+    }
+}
