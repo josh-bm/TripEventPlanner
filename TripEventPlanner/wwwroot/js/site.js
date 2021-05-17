8e7e8a46fd43ac4cb698f50717d6597f90bb50d7
Hong Duc Cai <hong.duc.cai@gmail.com>
2021-05-18 01:57:49 +0200

    ui on activity

--- TripEventPlanner/wwwroot/js/site.js
+++ TripEventPlanner/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const type = document.querySelectorAll(".activity");
+const activity = document.querySelectorAll(".form__activity");
+
+type.forEach(button => {
+
+    button.addEventListener("click", e => {
+        activity[0].value = button.textContent;
+    })
+
+})
