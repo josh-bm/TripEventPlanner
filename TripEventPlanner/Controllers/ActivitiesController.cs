c2a97642cf116584c766d84b06bece43dc594084
joshbm-git <joshbm.dev@gmail.com>
2021-05-19 11:46:58 +0200

    Cleaned up code

--- TripEventPlanner/Controllers/ActivitiesController.cs
+++ TripEventPlanner/Controllers/ActivitiesController.cs
@@ -9,16 +9,27 @@ using TripEventPlanner.Data;
 using TripEventPlanner.Models;
 using Microsoft.AspNetCore.Http;
 
-namespace TripEventPlanner.Controllers {
-    public class ActivitiesController : Controller {
+namespace TripEventPlanner.Controllers
+{
+    public class ActivitiesController : Controller
+    {
         private readonly ItravelPlannerDBContext _context;
 
-        public ActivitiesController( ItravelPlannerDBContext context ) {
+        public ActivitiesController(ItravelPlannerDBContext context)
+        {
             _context = context;
         }
 
-        public async Task<IActionResult> Index( string searchString, string activityType, string location) {
-
+        public async Task<IActionResult> Index(string searchString, string activityType, string location)
+        {
+            if (location == null)
+            {
+                location = "All";
+            }
+            if (activityType == null)
+            {
+                activityType = "All";
+            }
             ViewData["CurrentFilter"] = searchString;
 
             var activityTypeData = _context.ActivityTypes;
@@ -48,49 +59,56 @@ namespace TripEventPlanner.Controllers {
                 .Include(a => a.Location)
                 .Include(s => s.ActivityType);
 
-
-            if( String.IsNullOrEmpty(searchString) & activityType != "All" & location != "All" ) {
+            if (String.IsNullOrEmpty(searchString) & activityType != "All" & location != "All")
+            {
                 activity = queryAll;
             }
-            if( String.IsNullOrEmpty(searchString) & activityType == "All" & location != "All" ) {
+            if (String.IsNullOrEmpty(searchString) & activityType == "All" & location != "All")
+            {
                 activity = queryLocation;
             }
-            if( String.IsNullOrEmpty(searchString) & activityType != "All" & location == "All" ) {
+            if (String.IsNullOrEmpty(searchString) & activityType != "All" & location == "All")
+            {
                 activity = queryType;
             }
-            if( activityType == "All" & location == "All" & stringEmty ) {
+            if (activityType == "All" & location == "All" & stringEmty)
+            {
                 activity = _context.Activities
                     .Where(s => s.Name.Contains(searchString))
                     .Include(a => a.Location)
                     .Include(s => s.ActivityType);
             }
 
-            if( activityType != "All" & location != "All" & stringEmty ) {
+            if (activityType != "All" & location != "All" & stringEmty)
+            {
                 activity = _context.Activities
                     .Where(s => s.Name.Contains(searchString) && s.ActivityType.Name.Contains(activityType))
                     .Include(a => a.Location).Where(s => location == s.Location.Name)
                     .Include(s => s.ActivityType);
             }
-            if( activityType != "All" & location == "All" & stringEmty ) {
+            if (activityType != "All" & location == "All" & stringEmty)
+            {
                 activity = _context.Activities
                     .Where(s => s.Name.Contains(searchString) && s.ActivityType.Name.Contains(activityType))
                     .Include(a => a.Location)
                     .Include(s => s.ActivityType);
             }
-            if( activityType == "All" & location != "All" & stringEmty ) {
+            if (activityType == "All" & location != "All" & stringEmty)
+            {
                 activity = _context.Activities
                     .Where(s => s.Name.Contains(searchString) && s.Location.Name.Contains(location))
                     .Include(a => a.Location)
                     .Include(s => s.ActivityType);
             }
 
-
             return View(await activity.AsNoTracking().ToListAsync());
         }
 
-            // GET: Activities/Details/5
-            public async Task<IActionResult> Details( short? id ) {
-            if( id == null ) {
+        // GET: Activities/Details/5
+        public async Task<IActionResult> Details(short? id)
+        {
+            if (id == null)
+            {
                 return NotFound();
             }
 
@@ -98,7 +116,8 @@ namespace TripEventPlanner.Controllers {
                 .Include(a => a.ActivityType)
                 .Include(a => a.Location)
                 .FirstOrDefaultAsync(m => m.ActivityId == id);
-            if( activity == null ) {
+            if (activity == null)
+            {
                 return NotFound();
             }
 
@@ -106,17 +125,21 @@ namespace TripEventPlanner.Controllers {
         }
 
         // GET: Activities/Create
-        public IActionResult Create() {
+        public IActionResult Create()
+        {
             ViewData["ActivityTypeId"] = new SelectList(_context.ActivityTypes, "ActivityTypeId", "Name");
             ViewData["LocationId"] = new SelectList(_context.Locations, "LocationId", "Name");
             return View();
         }
-        [HttpPost]
-        public async Task<IActionResult> Create2(string ActivityTypeId, string Name, string Description, 
-            string Address, string Price, string ImageUrl, string LocationId) {
 
-            if (ModelState.IsValid) {
-                _context.Activities.Add( new Activity {
+        [HttpPost]
+        public async Task<IActionResult> Create2(string ActivityTypeId, string Name, string Description,
+            string Address, string Price, string ImageUrl, string LocationId)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Activities.Add(new Activity
+                {
                     ActivityTypeId = 1,
                     Name = Name,
                     Description = Description,
@@ -132,15 +155,18 @@ namespace TripEventPlanner.Controllers {
             }
             return View();
         }
+
         // POST: Activities/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create( 
-            [Bind("ActivityId,Name,Description,Address,Price,ActivityTypeId,ImageUrl,StartDate,EndDate,LocationId")] 
-        Activity activity ) {
-            if( ModelState.IsValid ) {
+        public async Task<IActionResult> Create(
+            [Bind("ActivityId,Name,Description,Address,Price,ActivityTypeId,ImageUrl,StartDate,EndDate,LocationId")]
+        Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
                 _context.Add(activity);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -151,13 +177,16 @@ namespace TripEventPlanner.Controllers {
         }
 
         // GET: Activities/Edit/5
-        public async Task<IActionResult> Edit( short? id ) {
-            if( id == null ) {
+        public async Task<IActionResult> Edit(short? id)
+        {
+            if (id == null)
+            {
                 return NotFound();
             }
 
             var activity = await _context.Activities.FindAsync(id);
-            if( activity == null ) {
+            if (activity == null)
+            {
                 return NotFound();
             }
             ViewData["ActivityTypeId"] = new SelectList(_context.ActivityTypes, "ActivityTypeId", "Name", activity.ActivityTypeId);
@@ -170,20 +199,28 @@ namespace TripEventPlanner.Controllers {
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit( short id, [Bind("ActivityId,Name,Description,Address,Price,ActivityTypeId,ImageUrl,StartDate,EndDate,LocationId")] Activity activity ) {
-            if( id != activity.ActivityId ) {
+        public async Task<IActionResult> Edit(short id, [Bind("ActivityId,Name,Description,Address,Price,ActivityTypeId,ImageUrl,StartDate,EndDate,LocationId")] Activity activity)
+        {
+            if (id != activity.ActivityId)
+            {
                 return NotFound();
             }
 
-            if( ModelState.IsValid ) {
-                try {
+            if (ModelState.IsValid)
+            {
+                try
+                {
                     _context.Update(activity);
                     await _context.SaveChangesAsync();
                 }
-                catch( DbUpdateConcurrencyException ) {
-                    if( !ActivityExists(activity.ActivityId) ) {
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActivityExists(activity.ActivityId))
+                    {
                         return NotFound();
-                    } else {
+                    }
+                    else
+                    {
                         throw;
                     }
                 }
@@ -195,8 +232,10 @@ namespace TripEventPlanner.Controllers {
         }
 
         // GET: Activities/Delete/5
-        public async Task<IActionResult> Delete( short? id ) {
-            if( id == null ) {
+        public async Task<IActionResult> Delete(short? id)
+        {
+            if (id == null)
+            {
                 return NotFound();
             }
 
@@ -204,7 +243,8 @@ namespace TripEventPlanner.Controllers {
                 .Include(a => a.ActivityType)
                 .Include(a => a.Location)
                 .FirstOrDefaultAsync(m => m.ActivityId == id);
-            if( activity == null ) {
+            if (activity == null)
+            {
                 return NotFound();
             }
 
@@ -214,14 +254,16 @@ namespace TripEventPlanner.Controllers {
         // POST: Activities/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed( short id ) {
+        public async Task<IActionResult> DeleteConfirmed(short id)
+        {
             var activity = await _context.Activities.FindAsync(id);
             _context.Activities.Remove(activity);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool ActivityExists( short id ) {
+        private bool ActivityExists(short id)
+        {
             return _context.Activities.Any(e => e.ActivityId == id);
         }
     }
